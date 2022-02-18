d30c7668edd087fe695a2ac2c0089983e570d472
Sayan Mitra <sayaniter@gmail.com>
2022-02-18 20:33:54 +0530

    style Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,24 @@
 const Error = () => {
   return (
     <>
-      <h1> Error 404. Page not found </h1>
+      <section
+        className="error"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Error 404. Page Not Found </h1>
+        <Link
+          to="/"
+          style={{ backgroundColor: "00b392", padding: 10, color: "#fff" }}
+        >
+          {" "}
+          Homepage{" "}
+        </Link>
+      </section>
     </>
   );
 };
